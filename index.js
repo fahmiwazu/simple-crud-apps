1f1cd418ab5b3d900b3b0dbceecf8a421ee9cbb5
waz <fahmiwazu@gmail.com>
2025-09-25 10:37:01 +0700

    FEAT: Add Credential handler for MonggoDB URI Connection setting

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Load environment variables
+require('dotenv').config();
+
 const express = require('express')
 const mongoose = require('mongoose');
 const Product = require('./models/product.models.js');
@@ -15,7 +18,18 @@ app.use("/api/products", productRoute);
 
 
 // Setup Connection for MonggoDB
-mongoose.connect("mongodb+srv://${{secret.SCA_DB_NAME}}:${{secret.SCA_DB_PASSWORD}}@backenddb.vgtoqfs.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+const dbName = process.env.SCA_DB_NAME;
+const dbPassword = process.env.SCA_DB_PASSWORD ;
+
+// Check if environment variables are loaded
+if (!dbName || !dbPassword) {
+    console.error("Database credentials not found in environment variables");
+    process.exit(1);
+}
+
+const mongoURI = `mongodb+srv://${dbName}:${dbPassword}@backenddb.vgtoqfs.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`;
+
+mongoose.connect(mongoURI)
     .then(() => {
         console.log("Connected to database!");
         app.listen(3000, () => {
